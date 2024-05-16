8d619816f10a87052a8d5b4b740aa83201ae2a94
Ana Julia <anajuliabit@gmail.com>
2024-05-16 18:44:08 -0300

    Add ArbitrumProposal_02.sol

---
+++ src/mocks/MockProxyUpgradeAction.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IProxyAdmin} from "@forge-proposal-simulator/src/interface/IProxyAdmin.sol";
+
+/// @title ProxyUpgradeAction
+/// @dev Arbitrum upgrades must be done through a delegate call to a GAC deployed contract
+contract MockProxyUpgradeAction {
+    function perform(
+        address admin,
+        address payable target,
+        address newLogic
+    ) public payable {
+        IProxyAdmin(admin).upgrade(target, newLogic);
+    }
+}
